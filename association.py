d3ec9df746b2b384f871c28ae39d669cadb6e721
Yanbei Chen <yanbeic@gmail.com>
2018-08-26 18:41:43 +0100

    update

--- association.py
+++ association.py
@@ -14,17 +14,13 @@ FLAGS = tf.app.flags.FLAGS
 
 # define one line functions for
 # computing distances, losses and normalisation
-pairwise_distance = lambda f1, f2, reduce_dim: \
-    tf.reduce_sum(tf.square(tf.subtract(f1, f2)), reduce_dim)
+pairwise_distance = lambda f1, f2, dim: tf.reduce_sum(tf.square(tf.subtract(f1, f2)), dim)
 
-hinge_loss = lambda dist_pos, dist_neg: \
-    tf.reduce_mean(tf.maximum(dist_pos - dist_neg + FLAGS.margin, 0))
+hinge_loss = lambda dist_pos, dist_neg: tf.reduce_mean(tf.maximum(dist_pos - dist_neg + FLAGS.margin, 0))
 
-l2_norm = lambda x: \
-    tf.nn.l2_normalize(x, 1, 1e-10)
+l2_norm = lambda x: tf.nn.l2_normalize(x, 1, 1e-10)
 
-normalize = lambda v: \
-    v ** 2 / (np.sum(v ** 2, 1, keepdims=True))
+normalize = lambda v: v ** 2 / (np.sum(v ** 2, 1, keepdims=True))
 
 
 def init_anchor(anchors_name, cam, num_trackets):
@@ -79,21 +75,19 @@ def association_loss(features_cam, intra_anchors_n, cross_anchors_n, labels_cam)
     dist_pos_intra = []
     dist_pos_cross = []
     dist_neg = []
+
     # loop under each camera to calculate association loss
     for i in range(FLAGS.num_cams):
         # compute the distance to the positive pairs:
         # corresponding intra-anchors (under the same camera)
-        dist_pos_intra.append(pairwise_distance(
-            features_cam[i], tf.gather(intra_anchors_n[i], labels_cam[i]), 1))
+        dist_pos_intra.append(pairwise_distance(features_cam[i], tf.gather(intra_anchors_n[i], labels_cam[i]), 1))
 
         # compute the distance to the positive pairs:
         # corresponding cross-anchors (cross the other camera(s))
-        dist_pos_cross.append(pairwise_distance(
-            features_cam[i], tf.gather(cross_anchors_n[i], labels_cam[i]), 1))
+        dist_pos_cross.append(pairwise_distance(features_cam[i], tf.gather(cross_anchors_n[i], labels_cam[i]), 1))
 
         # tracklet association ranking to compute distance to negative pairs
-        dist_neg.append(association_ranking(
-            features_cam[i], labels_cam[i], intra_anchors_n[i]))
+        dist_neg.append(association_ranking(features_cam[i], labels_cam[i], intra_anchors_n[i]))
 
     # compute two association losses
     dist_pos_intra = tf.concat(dist_pos_intra, 0)
@@ -110,8 +104,7 @@ def association_loss(features_cam, intra_anchors_n, cross_anchors_n, labels_cam)
 
 def association_ranking(features_cam, labels_cam, intra_anchors_n):
     # compute the distances to all anchors under the same camera
-    dist_all = pairwise_distance(
-        features_cam[:, tf.newaxis], intra_anchors_n, 2)
+    dist_all = pairwise_distance(features_cam[:, tf.newaxis], intra_anchors_n, 2)
 
     with tf.device('/cpu:0'): # place ranking on cpu
         dist_min1, rank1 = tf.nn.top_k(-dist_all, k=1, sorted=True)
@@ -125,12 +118,11 @@ def association_ranking(features_cam, labels_cam, intra_anchors_n):
     return dist_neg
 
 
-def cyclic_ranking(intra_anchors_batch_n, same_anchors_n,
-                   other_anchors_n, labels_cam, start_sign):
+def cyclic_ranking(intra_anchors_batch_n, same_anchors_n, other_anchors_n,
+                   labels_cam, start_sign):
     # perform cyclic ranking to discover similar tracklets across cameras
     # (1) rank to the other camera
-    dist = pairwise_distance(
-        intra_anchors_batch_n[:, tf.newaxis], other_anchors_n, 2)
+    dist = pairwise_distance(intra_anchors_batch_n[:, tf.newaxis], other_anchors_n, 2)
 
     with tf.device('/cpu:0'): # place ranking on cpu
         _, rank1 = tf.nn.top_k(-dist, k=1, sorted=True)
@@ -139,36 +131,31 @@ def cyclic_ranking(intra_anchors_batch_n, same_anchors_n,
     rank1_anchors = tf.gather(other_anchors_n, tf.squeeze(rank1, squeeze_dims=1))
 
     # (2) rank back to the original camera
-    dist = pairwise_distance(
-        rank1_anchors[:, tf.newaxis], same_anchors_n, 2)
+    dist = pairwise_distance(rank1_anchors[:, tf.newaxis], same_anchors_n, 2)
 
     with tf.device('/cpu:0'): # place ranking on cpu
         _, rank1 = tf.nn.top_k(-dist, k=1, sorted=True)
 
     # (3) consistency condition
-    consistent = tf.cast(tf.equal(tf.cast(labels_cam, dtype=tf.int32),
-                                  tf.squeeze(rank1, squeeze_dims=1)), tf.int32)
+    consistent = tf.cast(tf.equal(tf.cast(labels_cam, dtype=tf.int32), tf.squeeze(rank1, squeeze_dims=1)), tf.int32)
     consistent = tf.cast(consistent * tf.cast(start_sign, tf.int32), tf.bool)
 
     return consistent, rank1_anchors
 
 
-def update_intra_anchor(intra_anchors, intra_anchors_n,
-                        features_cam, labels_cam):
+def update_intra_anchor(intra_anchors, intra_anchors_n, features_cam, labels_cam):
     # update intra-anchor for each camera
     for i in range(FLAGS.num_cams):
         # compute the difference between old anchors and the new given data
-        diff = \
-            tf.gather(intra_anchors_n[i], labels_cam[i]) - features_cam[i]
+        diff = tf.gather(intra_anchors_n[i], labels_cam[i]) - features_cam[i]
         # update the intra-anchors under each camera
-        intra_anchors[i] = \
-            tf.scatter_sub(intra_anchors[i], labels_cam[i], FLAGS.eta * diff)
+        intra_anchors[i] = tf.scatter_sub(intra_anchors[i], labels_cam[i], FLAGS.eta * diff)
 
     return intra_anchors
 
 
-def update_cross_anchor(cross_anchors, intra_anchors_n,
-                        intra_anchors_batch_n, labels_cam, start_sign):
+def update_cross_anchor(cross_anchors, intra_anchors_n, intra_anchors_batch_n,
+                        labels_cam, start_sign):
     # update cross-anchor
     for i in range(FLAGS.num_cams):
         # other_anchors: all the anchors under other cameras
@@ -177,18 +164,14 @@ def update_cross_anchor(cross_anchors, intra_anchors_n,
         other_anchors_n = tf.concat(other_anchors_n, 0)
 
         consistent, rank1_anchors = \
-        cyclic_ranking(intra_anchors_batch_n[i], intra_anchors_n[i],
-                       other_anchors_n, labels_cam[i], start_sign)
+            cyclic_ranking(intra_anchors_batch_n[i], intra_anchors_n[i], other_anchors_n, labels_cam[i], start_sign)
 
-        # if the consistency fufills, update by
-        # merging using the best-matched anchors in anther camera
-        update = tf.where(consistent,
-                          (intra_anchors_batch_n[i] + rank1_anchors) / 2,
-                          intra_anchors_batch_n[i])
+        # if the consistency fulfills, update by
+        # merging with the best-matched rank1 anchors in another camera
+        update = tf.where(consistent, (intra_anchors_batch_n[i] + rank1_anchors) / 2, intra_anchors_batch_n[i])
 
         # update the associate centers under each camera
-        cross_anchors[i] = \
-            tf.scatter_update(cross_anchors[i], labels_cam[i], update)
+        cross_anchors[i] = tf.scatter_update(cross_anchors[i], labels_cam[i], update)
 
     return cross_anchors
 
@@ -217,6 +200,7 @@ def learning_graph(features, labels, cams, reuse_variables, start_sign):
     features_cam = []
     intra_anchors_n = []
     cross_anchors_n = []
+
     for i in range(FLAGS.num_cams):
         # A list of boolean variable denotes the indices that contain camera i
         condition_cam = tf.equal(cams, i)
@@ -230,12 +214,10 @@ def learning_graph(features, labels, cams, reuse_variables, start_sign):
         cross_anchors_n.append(l2_norm(cross_anchors[i]))
 
     # compute two association losses
-    final_loss = association_loss(
-        features_cam, intra_anchors_n, cross_anchors_n, labels_cam)
+    final_loss = association_loss(features_cam, intra_anchors_n, cross_anchors_n, labels_cam)
 
     # update the intra-anchors
-    intra_anchors = update_intra_anchor(
-        intra_anchors, intra_anchors_n, features_cam, labels_cam)
+    intra_anchors = update_intra_anchor(intra_anchors, intra_anchors_n, features_cam, labels_cam)
 
     # re-obtain the updated intra-anchors
     intra_anchors_n = []
@@ -245,7 +227,6 @@ def learning_graph(features, labels, cams, reuse_variables, start_sign):
         intra_anchors_batch_n.append(tf.gather(intra_anchors_n[i], labels_cam[i]))
 
     # update the cross-anchors
-    cross_anchors = update_cross_anchor(
-        cross_anchors, intra_anchors_n, intra_anchors_batch_n, labels_cam, start_sign)
+    cross_anchors = update_cross_anchor(cross_anchors, intra_anchors_n, intra_anchors_batch_n, labels_cam, start_sign)
 
     return final_loss, [intra_anchors, cross_anchors]
